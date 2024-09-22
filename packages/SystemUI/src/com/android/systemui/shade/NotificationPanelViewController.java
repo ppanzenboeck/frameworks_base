cb907d20560d6829f007e7f7f2753d1c103bd6cf
ppanzenboeck <panz.development@gmail.com>
2024-09-22 22:29:00 +0200

    Revert "SystemUI: IslandView: Ensure global headsup is enabled"
    This reverts commit 31cb3d317c58ea081e1dc309dfc37e4c8cc7f3db.

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -289,8 +289,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             "system:" + Settings.System.NOTIFICATION_MATERIAL_DISMISS;
     private static final String ISLAND_NOTIFICATION =
             "system:" + Settings.System.ISLAND_NOTIFICATION;
-    private static final String HEADS_UP_NOTIFICATIONS_ENABLED =
-            "global:" + Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED;
 
     private static final Rect M_DUMMY_DIRTY_RECT = new Rect(0, 0, 1, 1);
     private static final Rect EMPTY_RECT = new Rect();
@@ -642,7 +640,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     private IslandView mNotifIsland;
     private NotificationStackScrollLayout mNotificationStackScroller;
     private boolean mUseIslandNotification;
-    private boolean mUseHeadsUp;
 
     private final Runnable mFlingCollapseRunnable = () -> fling(0, false /* expand */,
             mNextCollapseSpeedUpFactor, false /* expandBecauseOfFalsing */);
@@ -4746,7 +4743,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             mDoubleTapToSleepObserver.onChange(true);
             mTunerService.addTunable(this, NOTIFICATION_MATERIAL_DISMISS);
             mTunerService.addTunable(this, ISLAND_NOTIFICATION);
-            mTunerService.addTunable(this, HEADS_UP_NOTIFICATIONS_ENABLED);
             // Theme might have changed between inflating this view and attaching it to the
             // window, so
             // force a call to onThemeChanged
@@ -4778,11 +4774,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                     break;
                 case ISLAND_NOTIFICATION:
                     mUseIslandNotification = TunerService.parseIntegerSwitch(newValue, false);
-                    mNotifIsland.setIslandEnabled(mUseIslandNotification && mUseHeadsUp);
-                    break;
-                case HEADS_UP_NOTIFICATIONS_ENABLED:
-                    mUseHeadsUp = TunerService.parseIntegerSwitch(newValue, true);
-                    mNotifIsland.setIslandEnabled(mUseIslandNotification && mUseHeadsUp);
+                    mNotifIsland.setIslandEnabled(mUseIslandNotification);
                     break;
                 default:
                     break;
@@ -5470,15 +5462,12 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
 
     @Override
     public void showIsland(boolean show) {
-        if (mUseIslandNotification && mUseHeadsUp) {
-            mNotifIsland.showIsland(show, getExpandedFraction());
-        }
+        if (!mUseIslandNotification) return;
+        mNotifIsland.showIsland(show, getExpandedFraction());
     }
 
     protected void updateIslandVisibility() {
-        if (mUseIslandNotification && mUseHeadsUp) {
-            mNotifIsland.updateIslandVisibility(getExpandedFraction());
-        }
+        mNotifIsland.updateIslandVisibility(getExpandedFraction());
     }
 
     @Override
